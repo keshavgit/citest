eb69649a4253709a77f5ba2edbe03d966128385f
keshavgit <vemulakeshav86@gmail.com>
2015-01-20 18:55:58 +0530

    Update TestWebDemo.java

--- src/test/java/com/ci/TestWebDemo.java
+++ src/test/java/com/ci/TestWebDemo.java
@@ -2,21 +2,31 @@ package com.ci;
 
 import static org.junit.Assert.*;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TestWebDemo {
 	WebDriver driver;
 	@Before
-	public void setUp()
+	public void setUp() throws Exception
 	{
-		driver=new FirefoxDriver(); 
+//		driver=new FirefoxDriver(); 
+		DesiredCapabilities cap = new DesiredCapabilities();
+    	cap = DesiredCapabilities.firefox();
+		driver = new RemoteWebDriver(new URL("http://172.168.0.114:5555/wd/hub"),cap);
 	}
 	@Test
-	public void testWebDemo() throws InterruptedException
+	public void testWebDemo() throws InterruptedException, Exception
 	{
+		
+//		driver.get("https://www.axisbank.co.in/");
 		driver.get("https://www.google.co.in/");
 		assertEquals("Google",driver.getTitle());
 		driver.findElement(By.id("gbqfq")).sendKeys("Selenium");
@@ -28,5 +38,4 @@ public class TestWebDemo {
 	{
 		driver.quit();
 	}
-
 }
