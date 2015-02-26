468c60fbc3e014e2dd726fa3f26d937389acd20b
keshavgit <vemulakeshav86@gmail.com>
2015-02-26 14:36:52 +0530

    Update TestWebDemo.java

--- src/test/java/com/ci/TestWebDemo.java
+++ src/test/java/com/ci/TestWebDemo.java
@@ -18,6 +18,7 @@ public class TestWebDemo {
 	public void setUp() throws Exception
 	{
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		cap.setPlatform(Platform.WINDOWS);
 		cap.setCapability("jenkins.label","redhat5 && amd64");
 		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
 		// driver = new RemoteWebDriver(new URL("http://172.168.0.114:5555/wd/hub"),cap);
